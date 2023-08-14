34098e514341c51d8fa54bf6d274005daff885bd
= <walter.kivindu@tracom.co.ke>
2023-08-14 10:29:48 +0300

    GraphQl with Lilo

--- src/main/java/com/example/lilogateway/config/LiloGatewayController.java
+++ src/main/java/com/example/lilogateway/config/LiloGatewayController.java
@@ -1,2 +1,30 @@
-package com.example.lilogateway.config;public class LiloGatewayController {
+package com.example.lilogateway.config;
+
+import io.fria.lilo.GraphQLRequest;
+import io.fria.lilo.Lilo;
+import io.fria.lilo.RemoteSchemaSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class LiloGatewayController {
+    private final Lilo lilo;
+
+    public LiloGatewayController() {
+        this.lilo = Lilo.
+                builder()
+                .addSource(RemoteSchemaSource.create("server1", "http://localhost:8081/graphql"))
+                .addSource(RemoteSchemaSource.create("server2", "http://localhost:8082/graphql"))
+                .build();
+    }
+
+    @ResponseBody
+    @PostMapping("/graphql")
+    public Map<String, Object> stitch(@RequestBody final GraphQLRequest request) {
+        return this.lilo.stitch(request.toExecutionInput()).toSpecification();
+    }
 }
